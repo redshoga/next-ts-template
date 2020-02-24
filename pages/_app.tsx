e87c41efb3d991248f36b72b0cf9f58be7c80bec
redshoga <yui.ichioka.contact@gmail.com>
2020-02-24 20:03:15 +0900

    Add React.StrictMode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
+import React from "react";
 import { AppProps } from 'next/app'
 import "../assets/styles/global.scss"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <React.StrictMode>
+    <Component {...pageProps} />
+  </React.StrictMode>
 }
 
 export default MyApp
