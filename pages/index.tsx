aaa21c84043f61b359618a321e4990f0c3d740e2
redshoga <yui.ichioka.contact@gmail.com>
2020-02-24 18:01:54 +0900

    最低限のTypeScriptの設定の追加

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default () => (
+  <div>
+    <Head>
+      <title>Create Next App</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+
+    <main>
+      hello next.js
+    </main>
+  </div>
+)
